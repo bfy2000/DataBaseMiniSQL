108d3cb64a8d38abc375fe8bc0a0db0c7ceae8d0
SHR0701 <50839514+SHR0701@users.noreply.github.com>
2020-06-06 12:47:14 +0800

    Add files via upload

--- src/RecordManager/Element.h
+++ src/RecordManager/Element.h
@@ -1,84 +1,81 @@
 #ifndef __ELEMENT_H__
 #define __ELEMENT_H__
-#include "../catalog_manager/FieldType.h"
 #include <string>
+
+#include "FieldType.h"
 
 /*定义了一个元素，包含其类型以及数据的存放，通过元算符重载设计实现不同的比较符运算*/
 class Element {
-public:
-	NumType type;
-	int m_int;
-	float m_float;
-	std::string m_char;
-	int length; // 注意 type=CHAR 时，这里的 length = 最大能保存字串转长度+1，因为最后一个是'\0'
-	// 否则 length = 4（针对 INT 和 FLOAT）
+ public:
+  NumType type;
+  int m_int;
+  float m_float;
+  std::string m_char;
+  int length;  // 注意 type=CHAR 时，这里的 length =
+               // 最大能保存字串转长度+1，因为最后一个是'\0'
+  // 否则 length = 4（针对 INT 和 FLOAT）
 
-	bool operator<(Element &e);
-	bool operator==(const Element &e);
-	bool operator>(Element &e);
-	bool operator>=(Element &e);
-	bool operator<=(Element &e);
-	bool operator!=(const Element &e);
-	
-	friend std::ostream& operator<<(std::ostream& out, Element &e);
+  bool operator<(Element &e);
+  bool operator==(const Element &e);
+  bool operator>(Element &e);
+  bool operator>=(Element &e);
+  bool operator<=(Element &e);
+  bool operator!=(const Element &e);
 
-	//Element(std::string s, int char_num);
-	Element(std::string s, int l = 1) {
-		type = CHAR;
-		m_char = s;
-		length = l;
-	}
-	Element(float f, int l = 4) {
-		type = FLOAT;
-		m_float = f;
-		length = l;
-	}
-	Element(int i, int l = 4) {
-		type = INT;
-		m_int = i;
-		length = l;
-	}
-	Element();
-	~Element();
+  friend std::ostream &operator<<(std::ostream &out, Element &e);
+  void setData(int i) { m_int = i; }
+  void setData(float f) { m_float = f; }
+  // Element(std::string s, int char_num);
+  Element(std::string s, int l = 1) {
+    type = CHAR;
+    m_char = s;
+    length = l;
+  }
+  Element(float f, int l = 4) {
+    type = FLOAT;
+    m_float = f;
+    length = l;
+  }
+  Element(int i, int l = 4) {
+    type = INT;
+    m_int = i;
+    length = l;
+  }
+  Element();
+  ~Element();
 
-	void setType(NumType t) { type = t; }
-	void setLength(int l) { length = l; }
+  void setType(NumType t) { type = t; }
+  void setLength(int l) { length = l; }
 
-	void elementToChar(char* c) {
-		if (type == INT) {//int长度是4字节
-			memcpy(c, &m_int, length);
-		}
-		else if (type == FLOAT) {//float长度是4字节
-			memcpy(c, &m_float, length);
-		}
-		else {//最长能够存储length个'a'
-			for (int i = 0; i < length; i++) {
-				c[i] = m_char[i];
-			}
-		}
-	}
-	void charToElement(char* c) {
-		if (type == INT) {//int长度是4字节
-			memcpy(&m_int, c, length);
-		}
-		else if (type == FLOAT) {//float长度是4字节
-			memcpy(&m_float, c, length);
-		}
-		else {//最长能够存储length个'a'
-			m_char = string(c, length);//但是存储的空间需要+1
-		}
-	}
-	void printElement() {
-		if (type == INT) {//int长度是4字节
-			cout << m_int;
-		}
-		else if (type == FLOAT) {//float长度是4字节
-			cout << m_float;
-		}
-		else {//最长能够存储length个'a'
-			cout << m_char;
-		}
-	}
+  void elementToChar(char *c) {
+    if (type == INT) {  // int长度是4字节
+      memcpy(c, &m_int, length);
+    } else if (type == FLOAT) {  // float长度是4字节
+      memcpy(c, &m_float, length);
+    } else {  //最长能够存储length个'a'
+      for (int i = 0; i < length; i++) {
+        c[i] = m_char[i];
+      }
+    }
+  }
+  void charToElement(char *c) {
+    if (type == INT) {  // int长度是4字节
+      memcpy(&m_int, c, length);
+    } else if (type == FLOAT) {  // float长度是4字节
+      memcpy(&m_float, c, length);
+    } else {                       //最长能够存储length个'a'
+      m_char = string(c, length);  //但是存储的空间需要+1
+    }
+  }
+  void printElement() {
+    if (type == INT) {  // int长度是4字节
+      cout << m_int;
+    } else if (type == FLOAT) {  // float长度是4字节
+      cout << m_float;
+    } else {  //最长能够存储length个'a'
+      cout << m_char;
+    }
+  }
 };
 
 #endif
--- src/RecordManager/RecordManager.h
+++ src/RecordManager/RecordManager.h
@@ -1,104 +1,100 @@
 #ifndef __RECORDMANAGER_H__
 #define __RECORDMANAGER_H__
-#include "base.h"
+#include "CatalogManager.h"
+#include "FieldType.h"
 #include "Table.h"
 #include "attribute.h"
-#include "FieldType.h"
-#include "CatalogManager.h"
+#include "base.h"
 
 class RecordManager {
-public:
-	//Table* isExistTable(string tableName);
-	//bool createTable(string tableName, string primaryKey, vector<Attribute> attributeVector);
-	//bool dropTable(string tableName);
-	bool insertValue(string tableName, Tuple tuple);
-	int deleteTuple(string tableName, vector<SelectCondition> selectConditions);
-	int deleteTuple(string tableName);
-	vector<Tuple> searchQuery(string tableName, vector<SelectCondition> selectConditions);
-	vector<Tuple> searchQuery(string tableName);
-	void PrintTable(Table *table) {
-		for (int i = 0; i < table->attributeVector.size(); i++) {
-			cout << table->attributeVector[i].attributeName << "\t";
-		}
-		cout << endl;
+ public:
+  // Table* isExistTable(string tableName);
+  // bool createTable(string tableName, string primaryKey, vector<Attribute>
+  // attributeVector); bool dropTable(string tableName);
+  bool insertValue(string tableName, Tuple tuple);
+  bool insertValue(string tableName,
+                   vector<pair<FieldType, string>> tupleString);
+  int deleteTuple(string tableName, vector<SelectCondition> selectConditions);
+  int deleteTuple(string tableName);
+  vector<Tuple> searchQuery(string tableName,
+                            vector<SelectCondition> selectConditions);
+  vector<Tuple> searchQuery(string tableName);
+  void PrintTable(Table* table) {
+    for (int i = 0; i < table->attributeVector.size(); i++) {
+      cout << table->attributeVector[i].attributeName << "\t";
+    }
+    cout << endl;
 
-		char* tmpChar = (char*)malloc(table->rowLength * sizeof(char));
-		if (tmpChar == NULL) {
-			cerr << "memory error" << endl;
-			return;
-		}
-		for (int i = 0; i < table->rowNum; i++) {
-			readData(table->tableName, "db_name?", i, tmpChar, table->rowLength, 0);
-			Tuple tuple = getTupleByIndex(table, i);
-			for (int j = 0; j < tuple.getData().size(); i++) {
-				tuple.getData()[j].printElement();
-				cout << "\t";
-			}
-			cout << endl;
-		}
-		free(tmpChar);
-	}
+    char* tmpChar = (char*)malloc(table->rowLength * sizeof(char));
+    if (tmpChar == NULL) {
+      cerr << "memory error" << endl;
+      return;
+    }
+    for (int i = 0; i < table->rowNum; i++) {
+      readData(table->tableName, "db_name?", i, tmpChar, table->rowLength, 0);
+      Tuple tuple = getTupleByIndex(table, i);
+      for (int j = 0; j < tuple.getData().size(); i++) {
+        tuple.getData()[j].printElement();
+        cout << "\t";
+      }
+      cout << endl;
+    }
+    free(tmpChar);
+  }
 
-private:
-	bool judgeCondition(Tuple tuple, SelectCondition condition);
-	bool isMatchTheAttribute(Table* table, Tuple* tuple);
-	bool isConflictTheUnique(Table* table, Tuple* tuple);
-	bool isConflict(Tuple* tuple1, Tuple* tuple2, int index);
-	Tuple charToTuple(Table *table, char* c) {
-		Tuple tuple;
-		//Tuple的数据存储格式
-		//Tuple中第一位0表示已经删除,1表示没有删除
-		//Tuple中第2-5位（4个整数）表示index
-		//Tuple中剩余位数表示数据
-		if (*c == '0') {
-			tuple.setIsDeleted(true);
-		}
-		else {
-			tuple.setIsDeleted(false);
-		}
-		c = c + sizeof(char);
+ private:
+  bool judgeCondition(Tuple tuple, SelectCondition condition);
+  bool isMatchTheAttribute(Table* table, Tuple* tuple);
+  bool isConflictTheUnique(Table* table, Tuple* tuple);
+  bool isConflict(Tuple* tuple1, Tuple* tuple2, int index);
+  Tuple charToTuple(Table* table, char* c) {
+    Tuple tuple;  // 0 1234   jsaoeprnvernvjnvjdfsvkl;df
+    if (*c == '0') {
+      tuple.setIsDeleted(true);
+    } else {
+      tuple.setIsDeleted(false);
+    }
+    c = c + sizeof(char);
 
-		int* tmp = (int*)malloc(sizeof(int));
-		memcpy(tmp, c, sizeof(int));
-		tuple.setIndex(*tmp);
-		free(tmp);
-		c = c + sizeof(int);
+    int* tmp = (int*)malloc(sizeof(int));
+    memcpy(tmp, c, sizeof(int));
+    tuple.setIndex(*tmp);
+    free(tmp);
+    c = c + sizeof(int);
 
-		int n = table->attributeNum;
-		for (int i = 0; i < n; i++) {
-			Element tmpElement;
-			tmpElement.setType(table->attributeVector[i].type.get_type());
-			tmpElement.setLength(table->attributeVector[i].type.get_length());
-			tmpElement.charToElement(c);
-			tuple.push_back_Data(tmpElement);
-			c = c + table->attributeVector[i].type.get_length();
-		}
-		return tuple;
-	}
-	void tupleToChar(Tuple tuple, char* c) {
-		//功能：将tuple拷贝进c地址
-		//使用此函数需要保证tuple是合理的，关键是string的长度合理
-		//使用此函数需要保证c的合理性
-		if (tuple.getIsDeleted() == true) {
-			*c = '0';
-		}
-		else {
-			*c = '1';
-		}
-		c = c + 1;
+    int n = table->attributeNum;
+    for (int i = 0; i < n; i++) {
+      Element tmpElement;
+      tmpElement.setType(table->attributeVector[i].type.get_type());
+      tmpElement.setLength(table->attributeVector[i].type.get_length());
+      tmpElement.charToElement(c);
+      tuple.push_back_Data(tmpElement);
+      c = c + table->attributeVector[i].type.get_length();
+    }
+    return tuple;
+  }
+  void tupleToChar(Tuple tuple, char* c) {
+    //功能：将tuple拷贝进c地址
+    //使用此函数需要保证tuple是合理的，关键是string的长度合理
+    //使用此函数需要保证c的合理性
+    if (tuple.getIsDeleted() == true) {
+      *c = '0';
+    } else {
+      *c = '1';
+    }
+    c = c + 1;
 
-		int t = tuple.getIndex();
-		memcpy(c, &(t), sizeof(int));
-		c = c + 4;
+    int t = tuple.getIndex();
+    memcpy(c, &(t), sizeof(int));
+    c = c + 4;
 
-		int n = tuple.getData().size();
-		for (int i = 0; i < n; i++) {
-			tuple.getData()[i].elementToChar(c);
-			c = c + tuple.getData()[i].length;
-		}
-	}
-	Tuple getTupleByIndex(Table* table, int index);
-
+    int n = tuple.getData().size();
+    for (int i = 0; i < n; i++) {
+      tuple.getData()[i].elementToChar(c);
+      c = c + tuple.getData()[i].length;
+    }
+  }
+  Tuple getTupleByIndex(Table* table, int index);
 };
 
 #endif
